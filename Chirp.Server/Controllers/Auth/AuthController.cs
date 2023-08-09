b90c4b6a1f9ee649973712455c008be560fa6dfd
j-junckes <me@junckes.dev>
2023-08-08 23:20:16 -0300

    feat(AuthController): add register, login, and forgot password endpoints
    - Add necessary using statements for JWT and Claims
    - Add required authorization middleware
    - Add logger, context, and configuration to the AuthController constructor
    - Implement the Register endpoint to register a new user
    - Implement the Login endpoint to authenticate a user
    - Implement the GenerateTokenAsync method to generate a JWT token
    - Implement the ForgotPassword endpoint to handle password recovery requests

--- Chirp.Server/Controllers/Auth/AuthController.cs
+++ Chirp.Server/Controllers/Auth/AuthController.cs
@@ -1,7 +1,12 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Chirp.Entities;
 using Chirp.Repository;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Chirp.Server.Controllers.Auth;
 
@@ -11,14 +16,17 @@ public class AuthController : ControllerBase
 {
   private readonly ILogger<AuthController> _logger;
   private readonly ChirpContext _context;
+  private readonly IConfiguration _config;
   
-  public AuthController(ILogger<AuthController> logger, ChirpContext context)
+  public AuthController(ILogger<AuthController> logger, ChirpContext context, IConfiguration config)
   {
     _logger = logger;
     _context = context;
+    _config = config;
   }
   
   [HttpPost("register", Name = "RegisterUser")]
+  [AllowAnonymous]
   public async Task<IActionResult> Register([FromBody] RegisterDto model, CancellationToken cToken)
   {
     await using var transaction = await _context.Database.BeginTransactionAsync(cToken);
@@ -70,10 +78,52 @@ public class AuthController : ControllerBase
   [HttpPost("login", Name = "LoginUser")]
   public async Task<IActionResult> Login([FromBody] LoginDto model, CancellationToken cToken)
   {
-    return Ok();
+    await using var transaction = await _context.Database.BeginTransactionAsync(cToken);
+    try
+    {
+      var account = await _context.Accounts.FirstOrDefaultAsync(x => x.Email == model.Email, cToken);
+      if (account == null)
+      {
+        return BadRequest(new { message = "Email is incorrect" });
+      }
+      
+      if (!BCrypt.Net.BCrypt.Verify(model.Password, account.Password))
+      {
+        return BadRequest(new { message = "Password is incorrect" });
+      }
+      
+      var token = await GenerateTokenAsync(account);
+      
+      return Ok(new { token });
+    }
+    catch (Exception ex)
+    {
+      await transaction.RollbackAsync(cToken);
+      _logger.LogError(ex, "Error while logging in user");
+      return BadRequest(new { message = "Error while logging in user" });
+    }
+  }
+  
+  private Task<string> GenerateTokenAsync(Account account)
+  {
+    var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"] ?? ""));
+    var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+    var claims = new[]
+    {
+      new Claim("sub", account.AccountId.ToString())
+    };
+
+    var token = new JwtSecurityToken(_config["Jwt:Issuer"] ?? "http://localhost:5000",
+      _config["Jwt:Audience"] ?? "http://localhost:5000",
+      claims,
+      expires: DateTime.Now.AddHours(1),
+      signingCredentials: credentials);
+
+    return Task.FromResult(new JwtSecurityTokenHandler().WriteToken(token));
   }
 
   [HttpPost("forgotPassword", Name = "ForgotPassword")]
+  [AllowAnonymous]
   public async Task<IActionResult> ForgotPassword([FromBody] ForgotPasswordDto model, CancellationToken cToken)
   {
     return Ok();
