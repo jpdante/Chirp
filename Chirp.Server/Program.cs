9b07084cc86975ca0947366de8b276bb73a30f73
j-junckes <me@junckes.dev>
2023-08-08 23:19:46 -0300

    feat(server): add JWT authentication to Chirp.Server
    - Add JWT authentication scheme using JwtBearerDefaults.AuthenticationScheme
    - Configure token validation parameters
    - Set issuer, audience, and signing key for token validation
    - Use UTF-8 encoding for SymmetricSecurityKey

--- Chirp.Server/Program.cs
+++ Chirp.Server/Program.cs
@@ -1,6 +1,9 @@
 using System.Reflection;
+using System.Text;
 using Chirp.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace Chirp.Server;
@@ -20,15 +23,57 @@ public static class Program
     {
       c.SwaggerDoc("v1", new OpenApiInfo { Title = "Chirp", Version = "v1" });
 
+      c.AddSecurityDefinition("Bearer",
+        new OpenApiSecurityScheme
+        {
+          Description = "JWT Authorization header using the Bearer scheme.\n\n" +
+                        "Example: 'Bearer 12345abcdef'",
+          Name = "Authorization",
+          In = ParameterLocation.Header,
+          Type = SecuritySchemeType.ApiKey,
+          Scheme = "Bearer"
+        });
+
+      c.AddSecurityRequirement(new OpenApiSecurityRequirement
+      {
+        {
+          new OpenApiSecurityScheme
+          {
+            Reference = new OpenApiReference
+            {
+              Type = ReferenceType.SecurityScheme,
+              Id = "Bearer"
+            },
+            Scheme = "oauth2",
+            Name = "Bearer",
+            In = ParameterLocation.Header,
+          },
+          new List<string>()
+        }
+      });
+
       var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
       var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
       c.IncludeXmlComments(xmlPath);
     });
 
     builder.Services.AddDbContext<ChirpContext>(options => options
-        .UseNpgsql(builder.Configuration.GetConnectionString("Default"), o => o.MigrationsAssembly("Chirp.Server"))
-        .UseSnakeCaseNamingConvention()
+      .UseNpgsql(builder.Configuration.GetConnectionString("Default"), o => o.MigrationsAssembly("Chirp.Server"))
+      .UseSnakeCaseNamingConvention()
     );
+    
+    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+    {
+      options.TokenValidationParameters = new TokenValidationParameters
+      {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issue"] ?? "http://localhost:5000",
+        ValidAudience = builder.Configuration["Jwt:Audience"] ?? "http://localhost:5000",
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"] ?? ""))
+      };
+    });
 
     var app = builder.Build();
 
