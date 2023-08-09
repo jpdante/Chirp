29c810728bb2be4a27bf9c642f7126940718fb69
j-junckes <me@junckes.dev>
2023-08-08 23:02:54 -0300

    feat(auth): add user registration endpoint
    - Import necessary dependencies and libraries
    - Define logger and context variables in AuthController
    - Implement Register method to handle user registration
    - Begin transaction and check if email and handle already exist
    - Create new account and profile objects
    - Add account and profile to context and save changes
    - Commit transaction and return success message
    - Handle exceptions and rollback transaction if necessary

--- Chirp.Server/Controllers/Auth/AuthController.cs
+++ Chirp.Server/Controllers/Auth/AuthController.cs
@@ -1,4 +1,7 @@
+using Chirp.Entities;
+using Chirp.Repository;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Chirp.Server.Controllers.Auth;
 
@@ -6,10 +9,62 @@ namespace Chirp.Server.Controllers.Auth;
 [Route("api/auth")]
 public class AuthController : ControllerBase
 {
+  private readonly ILogger<AuthController> _logger;
+  private readonly ChirpContext _context;
+  
+  public AuthController(ILogger<AuthController> logger, ChirpContext context)
+  {
+    _logger = logger;
+    _context = context;
+  }
+  
   [HttpPost("register", Name = "RegisterUser")]
   public async Task<IActionResult> Register([FromBody] RegisterDto model, CancellationToken cToken)
   {
-    return Ok();
+    await using var transaction = await _context.Database.BeginTransactionAsync(cToken);
+    try
+    {
+      if (await _context.Accounts.AnyAsync(x => x.Email == model.Email, cToken))
+      {
+        return BadRequest("Email already exists");
+      }
+      
+      if (await _context.Profiles.AnyAsync(x => x.Handle == model.Handle, cToken))
+      {
+        return BadRequest("Handle already exists");
+      }
+      
+      var account = new Account
+      {
+        Email = model.Email,
+        Password = BCrypt.Net.BCrypt.HashPassword(model.Password),
+        CreatedAt = DateTime.UtcNow,
+        LastUpdatedAt = DateTime.UtcNow
+      };
+      
+      await _context.Accounts.AddAsync(account, cToken);
+      await _context.SaveChangesAsync(cToken);
+      
+      var profile = new Profile
+      {
+        AccountId = account.AccountId,
+        Handle = model.Handle,
+        Name = model.Name
+      };
+      
+      await _context.Profiles.AddAsync(profile, cToken);
+      await _context.SaveChangesAsync(cToken);
+      
+      await transaction.CommitAsync(cToken);
+
+      return Ok(new { message = "User registered successfully" });
+    }
+    catch (Exception ex)
+    {
+      await transaction.RollbackAsync(cToken);
+      _logger.LogError(ex, "Error while registering user");
+      return BadRequest(new { message = "Error while registering user" });
+    }
   }
 
   [HttpPost("login", Name = "LoginUser")]
